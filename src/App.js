12664fe48596001bcb286986df5735c47b74bf2f
faragzz <a.khaled46462@gmail.com>
2023-05-05 19:31:54 +0300

    update css and html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div
+  style={{
+    margin:"auto",
+    width:800,
+    paddingTop:"1ren",
+  }}
+  >
+    <h1 className='title'>Pokemon Search</h1>
+    <table width='100%'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>helloz</td>
+          <td>helloz,Why me</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  
   );
 }
 
